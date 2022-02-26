4eb337971411b85d50187324cedb4e336a50f36b
Caique Santos <caique.santos11@fatec.sp.gov.br>
2022-02-25 21:32:41 -0300

    Teste

---
+++ aula.js
@@ -0,0 +1,17 @@
+const nome = "José"
+const idade = 27
+nome = "Maria"
+const a
+
+let a = 2
+let b = 'abc'
+let endereco = `Rua K, 12`
+
+var c = 2 + 3
+var d = "abcd"
+
+//declaração de variaveis (const, let e var)
+//Tipagem dinamica no js
+//Valores lógicos dos tipos no js
+//Comparação com coersão implicita
+//
