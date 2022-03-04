818875919cd6c2b2ba3fdf1904b2d4a6ada6306a
Caique Santos <caique.santos11@fatec.sp.gov.br>
2022-03-04 19:23:02 -0300

    Funções de Vetores

--- aula.js
+++ aula.js
@@ -1,17 +1,18 @@
-const nome = "José"
-const idade = 27
-nome = "Maria"
-const a
-
-let a = 2
-let b = 'abc'
-let endereco = `Rua K, 12`
-
-var c = 2 + 3
-var d = "abcd"
-
+//Aula 25/02
 //declaração de variaveis (const, let e var)
 //Tipagem dinamica no js
 //Valores lógicos dos tipos no js
 //Comparação com coersão implicita (== e ===)
-//
+
+//Aula 04/03
+const nomes = ["Ana Maria", "Antonio", "Rodrigo", "Alex", "Cristina"]
+const res = nomes.map((nome) => nome.charAt(0))
+//console.log(res)
+const comp = nomes.map((nome) => nome.length)
+//console.log(comp)
+const todosComecamComA = nomes.every(nome => nome.startsWith('A'))
+//console.log(todosComecamComA)
+
+const valores = [1, 2, 3, 4]
+const soma = valores.reduce((ac, v) => ac + v)
+//console.log(soma)
